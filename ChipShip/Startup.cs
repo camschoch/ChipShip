3977dab8ed1b25867ca6601cb32337e39620183c
camschoch <camschoch3@gmail.com>
2017-09-26 13:15:52 -0500

    Add project files.

---
+++ ChipShip/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ChipShip.Startup))]
+namespace ChipShip
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
